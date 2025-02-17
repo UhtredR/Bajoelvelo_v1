33535eb8055c04ebd904594d34939d5ed23e6f4a
Elie <Elie@DESKTOP-1II6R26>
2025-02-17 01:27:58 -0400

    Reorganización y eliminación de formularios
    Se han reorganizado los archivos de formularios en una nueva carpeta llamada `FORMS` y se han actualizado las referencias a los recursos incrustados. Se han eliminado los formularios `frmCatalogo`, `frmHelp`, `frmMainmenu` y `frmServices`, junto con sus archivos de diseño y recursos. Se ha añadido una nueva clase `FormFuncs` para funcionalidades de formularios y un nuevo formulario `frmInicio` con su diseño y lógica.

---
+++ Clases_Func/FormFuncs.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bajoelvelo_v1.Clases_Func
+{
+    public class FormFuncs
+    {
+       private Form formf;
+
+        public FormFuncs(Form form)
+        {
+            this.formf = form;
+        }
+
+        public void cerrarForm()
+        {
+            formf.Close();
+        }
+
+        public void minimizarForm()
+        {
+            formf.WindowState = FormWindowState.Minimized;
+        }
+
+        public void maximizarForm()
+        {
+            if (formf.WindowState == FormWindowState.Maximized)
+                formf.WindowState = FormWindowState.Normal;
+
+            else
+                formf.WindowState = FormWindowState.Maximized;
+        }
+    }
+}
